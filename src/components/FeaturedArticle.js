d0c289f0e002889e76163fd8e6d79ca30b89301b
Irtiza Hafiz <irtiza@MacBook-Pro.local>
2020-07-19 20:58:30 -0700

    center aligned blog content;scaling image appropriately without stretching

--- src/components/FeaturedArticle.js
+++ src/components/FeaturedArticle.js
@@ -7,10 +7,12 @@ export default function FeaturedArticle() {
       <p className="featured-title">
         Morning Routine That Completely Changed My Life
       </p>
-      <img
-        className="featured-image"
-        src="https://images.unsplash.com/photo-1582778562829-d017b9105852?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=4178&q=80"
-      />
+      <div className="image-container">
+        <img
+          className="featured-image"
+          src="https://images.unsplash.com/photo-1582778562829-d017b9105852?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=4178&q=80"
+        />
+      </div>
       <div className="featured-details">
         <p className="featured-cat">Productivity</p>
         <p className="featured-time">7 min read</p>
