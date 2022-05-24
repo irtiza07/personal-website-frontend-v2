ea80dc37ac2fa43406fb9317b382160b722581ea
Irtiza Hafiz <irtiza@irtiza.local>
2022-05-24 11:48:21 -0700

    update socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,18 @@ export default function Footer() {
     <div className="footer-container">
       <div className="footer-contact-container">
         <p className="socials-header">Get in touch</p>
-        <a className="socials-link">Instagram</a>
-        <a className="socials-link">Facebook</a>
-        <a className="socials-link">Github</a>
+        <a className="socials-link" href="http://irtizahafiz.medium.com">
+          Blog
+        </a>
+        <a className="socials-link" href="https://github.com/irtiza07">
+          Github
+        </a>
+        <a
+          className="socials-link"
+          href="https://www.instagram.com/irtiza.hafiz/"
+        >
+          Instagram
+        </a>
       </div>
     </div>
   );
